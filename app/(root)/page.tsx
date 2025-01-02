64caa9d2955ac829b079b488a5d7bd969e522f89
tassanai001 <tassanai.yeeton@gmail.com>
2025-01-02 20:15:28 +0700

    Seed Sample Data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,5 @@
 import ProductList from '@/components/shared/product/product-list';
-import sampleData from '@/sample-data/db/sample-data'
+import sampleData from '@/db/sample-data'
 
 export const metadata = {
   title: "Home",
