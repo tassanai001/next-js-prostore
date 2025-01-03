5b90aa9e628a83d6a34df1f17bfbf40d17bdbff0
tassanai001 <tassanai.yeeton@gmail.com>
2025-01-03 16:48:04 +0700

    Serverless Environment Config

--- lib/actions/product.action.ts
+++ lib/actions/product.action.ts
@@ -1,11 +1,10 @@
 "use server";
-import { PrismaClient } from "@prisma/client";
+import { prisma } from '@/db/prisma';
 import { convertToPlainObject } from "../utils";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
 
 // Get latest products
 export const getLatestProducts = async () => {
-  const prisma = new PrismaClient();
   const products = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: {
