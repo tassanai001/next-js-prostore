10a2318a95d4be77c1f1f3b8204ab70ecb09b060
tassanai001 <tassanai.yeeton@gmail.com>
2025-01-05 20:50:12 +0700

    Callback URL Redirect

--- app/(auth)/sign-in/credentials-signin-form.tsx
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -7,6 +7,7 @@ import Link from 'next/link';
 import { useActionState } from 'react';
 import { useFormStatus } from 'react-dom';
 import { signInWithCredentials } from '@/lib/actions/user.actions';
+import { useSearchParams } from 'next/navigation';
 
 const CredentialsSignInForm = () => {
 
@@ -14,6 +15,9 @@ const CredentialsSignInForm = () => {
         message: '',
         success: false,
     });
+
+    const searchParams = useSearchParams();
+    const callbackUrl = searchParams.get('callbackUrl') || '/';
 
     const SignInButton = () => {
         const { pending } = useFormStatus();
@@ -26,6 +30,7 @@ const CredentialsSignInForm = () => {
 
     return (
         <form action={action}>
+            <input type='hidden' name='callbackUrl' value={callbackUrl} />
             <div className='space-y-6'>
                 <div>
                     <Label htmlFor='email'>Email</Label>
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,37 +1,34 @@
-'use server';
+"use server";
 
 // import { isRedirectError } from 'next/dist/client/components/redirect';
-import { signIn, signOut } from '@/auth';
-import { signInFormSchema } from '../validator';
+import { signIn, signOut } from "@/auth";
+import { signInFormSchema } from "../validator";
+import { isRedirectError } from "next/dist/client/components/redirect-error";
 
 // Sign in the user with credentials
 export async function signInWithCredentials(
-    prevState: unknown,
-    formData: FormData
-  ) {
-    debugger
-    try {
-      const user = signInFormSchema.parse({
-        email: formData.get('email'),
-        password: formData.get('password'),
-      });
+  prevState: unknown,
+  formData: FormData
+) {
+  try {
+    const user = signInFormSchema.parse({
+      email: formData.get("email"),
+      password: formData.get("password"),
+    });
 
-      await signIn('credentials', user);
+    await signIn("credentials", user);
 
-      return { success: true, message: 'Signed in successfully' };
-    } catch (error) {
-        debugger;
-        console.log(error);
-
-    //   if (isRedirectError(error)) {
-    //     throw error;
-    //   }
-
-      return { success: false, message: 'Invalid email or password' };
+    return { success: true, message: "Signed in successfully" };
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error;
     }
+
+    return { success: false, message: "Invalid email or password" };
+  }
 }
 
 // Sign the user out
 export async function signOutUser() {
-    await signOut();
-  }
+  await signOut();
+}
