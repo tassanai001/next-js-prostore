c0e3c6787fa0790ea2f10f5061c84410d168234b
tassanai001 <tassanai.yeeton@gmail.com>
2025-01-05 20:06:51 +0700

    Credentials Sign In Form

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -6,3 +6,8 @@ export const APP_SERVER_URL =
   process.env.NEXT_PUBLIC_APP_SERVER_URL || "http://localhost:3000";
 export const LATEST_PRODUCTS_LIMIT =
   Number(process.env.NEXT_PUBLIC_LATEST_PRODUCTS_LIMIT) || 10;
+
+  export const signInDefaultValues = {
+    email: '',
+    password: '',
+  };
