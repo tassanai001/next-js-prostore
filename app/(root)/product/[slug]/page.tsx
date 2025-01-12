79f6d9dd3b656b5405ad2eec386a36b99b4207e0
tassanai001 <tassanai.yeeton@gmail.com>
2025-01-12 20:31:57 +0700

    Dynamic Cart Button

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -5,6 +5,7 @@ import { getProductBySlug } from '@/lib/actions/product.action';
 import { Badge } from '@/components/ui/badge';
 import ProductImages from '@/components/shared/product/product-images';
 import AddToCart from '@/components/shared/product/add-to-cart';
+import { getMyCart } from '@/lib/actions/cart.actions';
 
 const ProductDetailsPage = async (props: {
   params: Promise<{ slug: string }>;
@@ -16,6 +17,7 @@ const ProductDetailsPage = async (props: {
   const product = await getProductBySlug(slug);
   if (!product) notFound();
 
+  const cart = await getMyCart(); //👈 Add this line
   return (
     <>
       <section>
@@ -69,6 +71,7 @@ const ProductDetailsPage = async (props: {
                 {product.stock > 0 && (
                   <div className='flex-center'>
                     <AddToCart
+                      cart={cart}
                       item={{
                         productId: product.id,
                         name: product.name,
--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -3,12 +3,18 @@
 import { Button } from '@/components/ui/button';
 import { ToastAction } from '@/components/ui/toast';
 import { useToast } from '@/hooks/use-toast';
-import { addItemToCart } from '@/lib/actions/cart.actions';
-import { CartItem } from '@/types';
-import { Plus } from 'lucide-react';
+import { addItemToCart, removeItemFromCart } from '@/lib/actions/cart.actions';
+import { Cart, CartItem } from '@/types';
+import { Plus, Minus  } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 
-const AddToCart = ({ item }: { item: CartItem }) => {
+const AddToCart = ({
+    cart,
+    item,
+  }: {
+    cart?: Cart;
+    item: Omit<CartItem, 'cartId'>;
+  }) => {
 
     const router = useRouter();
     const { toast } = useToast();
@@ -40,12 +46,34 @@ const AddToCart = ({ item }: { item: CartItem }) => {
         });
     };
 
-    return (
+    // Remove item from cart
+    const handleRemoveFromCart = async () => {
+        const res = await removeItemFromCart(item.productId);
+        toast({
+            variant: res.success ? 'default' : 'destructive',
+            description: res.message,
+        });
+        return;
+    };
+
+    const existItem = cart && cart.items.find((x) => x.productId === item.productId);
+
+    return existItem ? (
+        <div>
+          <Button type='button' variant='outline' onClick={handleRemoveFromCart}>
+            <Minus className='w-4 h-4' />
+          </Button>
+          <span className='px-2'>{existItem.qty}</span>
+          <Button type='button' variant='outline' onClick={handleAddToCart}>
+            <Plus className='w-4 h-4' />
+          </Button>
+        </div>
+      ) : (
         <Button className='w-full' type='button' onClick={handleAddToCart}>
-            <Plus />
-            Add to cart
+          <Plus className='w-4 h-4' />
+          Add to cart
         </Button>
-    )
+      );
 };
 
 export default AddToCart;
