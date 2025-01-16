9aa989568dddbe57c984112f7fc0956e9a8ee659
tassanai001 <tassanai.yeeton@gmail.com>
2025-01-16 19:14:48 +0700

    Shipping Address Form

--- app/(root)/shipping-address/page.tsx
+++ app/(root)/shipping-address/page.tsx
@@ -3,29 +3,35 @@ import { getMyCart } from '@/lib/actions/cart.actions';
 import { getUserById } from '@/lib/actions/user.actions';
 import { Metadata } from 'next';
 import { redirect } from 'next/navigation';
+import ShippingAddressForm from './shipping-address-form';
+import { ShippingAddress } from '@/types';
 
 export const metadata: Metadata = {
-  title: 'Shipping Address',
+    title: 'Shipping Address',
 };
 
 const ShippingAddressPage = async () => {
-  const cart = await getMyCart();
+    const cart = await getMyCart();
 
-  if (!cart || cart.items.length === 0) redirect('/cart');
+    if (!cart || cart.items.length === 0) redirect('/cart');
 
-  const session = await auth();
+    const session = await auth();
 
-  const userId = session?.user?.id;
+    const userId = session?.user?.id;
 
-  if (!userId) {
-    throw new Error('User ID not found');
-  }
+    if (!userId) {
+        throw new Error('User ID not found');
+    }
 
-  const user = await getUserById(userId);
-  console.log(user);
+    const user = await getUserById(userId);
+    console.log(user);
 
 
-  return <>Shipping Page</>;
+    return (
+        <>
+            <ShippingAddressForm address={user.address as ShippingAddress} />
+        </>
+    )
 };
 
 export default ShippingAddressPage;
