56d3564789fb5ea7ac44148890fe8836429522ae
tassanai001 <tassanai.yeeton@gmail.com>
2025-01-20 22:25:56 +0700

    Order Zod Schemas & Type

--- types/index.ts
+++ types/index.ts
@@ -2,6 +2,8 @@ import { z } from 'zod';
 import {
     cartItemSchema,
     insertCartSchema,
+    insertOrderItemSchema,
+    insertOrderSchema,
     insertProductSchema,
     shippingAddressSchema,
   } from '@/lib/validator';
@@ -16,3 +18,14 @@ export type Product = z.infer<typeof insertProductSchema> & {
 export type Cart = z.infer<typeof insertCartSchema>;
 export type CartItem = z.infer<typeof cartItemSchema>;
 export type ShippingAddress = z.infer<typeof shippingAddressSchema>;
+export type OrderItem = z.infer<typeof insertOrderItemSchema>;
+export type Order = z.infer<typeof insertOrderSchema> & {
+  id: string;
+  createdAt: Date;
+  isPaid: boolean;
+  paidAt: Date | null;
+  isDelivered: boolean;
+  deliveredAt: Date | null;
+  orderItems: OrderItem[];
+  user: { name: string; email: string };
+};
