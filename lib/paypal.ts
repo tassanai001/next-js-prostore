e774ef1abe4ad248340016f092c22f04372fe30a
tassanai001 <tassanai.yeeton@gmail.com>
2025-01-30 23:06:53 +0700

    Create Order & Capture Payment Requests

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -1,33 +1,72 @@
-const base = process.env.PAYPAL_API_URL || 'https://api-m.sandbox.paypal.com';
+const base = process.env.PAYPAL_API_URL || "https://api-m.sandbox.paypal.com";
 
 export const paypal = {
-    base,
-    clientId: process.env.PAYPAL_CLIENT_ID,
-    clientSecret: process.env.PAYPAL_CLIENT_SECRET,
-    appSecret: process.env.PAYPAL_APP_SECRET
+  createOrder: async function createOrder(price: number) {
+    const accessToken = await generateAccessToken();
+    const url = `${base}/v2/checkout/orders`;
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({
+        intent: "CAPTURE",
+        purchase_units: [
+          {
+            amount: {
+              currency_code: "USD",
+              value: price,
+            },
+          },
+        ],
+      }),
+    });
+
+    return handleResponse(response);
+  },
+  capturePayment: async function capturePayment(orderId: string) {
+    const accessToken = await generateAccessToken();
+    const url = `${base}/v2/checkout/orders/${orderId}/capture`;
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    return handleResponse(response);
+  },
 };
 
 // Generate an access token for the PayPal API
 export async function generateAccessToken() {
-    const { PAYPAL_CLIENT_ID, PAYPAL_APP_SECRET } = process.env;
-    const auth = Buffer.from(`${PAYPAL_CLIENT_ID}:${PAYPAL_APP_SECRET}`).toString(
-      'base64'
-    );
+  const { PAYPAL_CLIENT_ID, PAYPAL_APP_SECRET } = process.env;
+  const auth = Buffer.from(`${PAYPAL_CLIENT_ID}:${PAYPAL_APP_SECRET}`).toString(
+    "base64"
+  );
 
-    const response = await fetch(`${base}/v1/oauth2/token`, {
-      method: 'POST',
-      body: 'grant_type=client_credentials',
-      headers: {
-        Authorization: `Basic ${auth}`,
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-    });
+  const response = await fetch(`${base}/v1/oauth2/token`, {
+    method: "POST",
+    body: "grant_type=client_credentials",
+    headers: {
+      Authorization: `Basic ${auth}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
 
-    if (response.ok) {
-      const jsonData = await response.json();
-      return jsonData.access_token;
-    } else {
-      const errorMessage = await response.text();
-      throw new Error(errorMessage);
-    }
+  const jsonData = await handleResponse(response);
+  return jsonData.access_token;
+}
+
+async function handleResponse(response: Response) {
+  if (response.ok) {
+    const jsonData = await response.json();
+    return jsonData;
+  } else {
+    const errorMessage = await response.text();
+    throw new Error(errorMessage);
   }
+}
