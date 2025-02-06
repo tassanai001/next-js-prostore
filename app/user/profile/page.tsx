e412e35c07ebead770216a71cfa9e11d270bde8c
tassanai001 <tassanai.yeeton@gmail.com>
2025-02-06 20:19:19 +0700

    User Profile Form

--- app/user/profile/page.tsx
+++ app/user/profile/page.tsx
@@ -1,5 +1,27 @@
-const ProfilePage = () => {
-    return <>Profile</>;
+import { auth } from '@/auth';
+import { Metadata } from 'next';
+import { SessionProvider } from 'next-auth/react';
+import { redirect } from 'next/navigation';
+import ProfileForm from './profile-form';
+
+export const metadata: Metadata = {
+    title: 'Customer Profile',
 };
 
-export default ProfilePage;
+export default async function ProfilePage() {
+
+    const session = await auth();
+
+    if (!session || !session.user) {
+        redirect('/sign-in');
+    }
+
+    return (
+        <SessionProvider session={session}>
+            <div className='max-w-md  mx-auto space-y-4'>
+                <h2 className='h2-bold'>Profile</h2>
+                <ProfileForm />
+            </div>
+        </SessionProvider>
+    )
+}
---
+++ app/user/profile/profile-form.tsx
@@ -0,0 +1,89 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { useToast } from '@/hooks/use-toast';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useSession } from 'next-auth/react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { updateProfileSchema } from '@/lib/validator';
+import { updateProfile } from '@/lib/actions/user.actions';
+
+const ProfileForm = () => {
+  const { data: session, update } = useSession();
+
+  const form = useForm<z.infer<typeof updateProfileSchema>>({
+    resolver: zodResolver(updateProfileSchema),
+    defaultValues: {
+      name: session?.user?.name ?? '',
+      email: session?.user?.email ?? '',
+    },
+  });
+
+  const { toast } = useToast();
+
+  const onSubmit = () => {
+    return;
+  }
+
+  return (
+    <Form {...form}>
+       <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col gap-5">
+        <div className='flex flex-col gap-5'>
+          <FormField
+            control={form.control}
+            name='email'
+            render={({ field }) => (
+              <FormItem className='w-full'>
+                <FormControl>
+                  <Input
+                    disabled
+                    placeholder='Email'
+                    {...field}
+                    className='input-field'
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name='name'
+            render={({ field }) => (
+              <FormItem className='w-full'>
+                <FormControl>
+                  <Input
+                    placeholder='Name'
+                    {...field}
+                    className='input-field'
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <Button
+          type='submit'
+          size='lg'
+          disabled={form.formState.isSubmitting}
+          className='button col-span-2 w-full'
+        >
+          {form.formState.isSubmitting ? 'Submitting...' : 'Update Profile'}
+        </Button>
+      </form>
+    </Form>
+  );
+};
+export default ProfileForm;
