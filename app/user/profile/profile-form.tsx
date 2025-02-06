b9af83f14c3d7195dbf15a7669a58ee43bff59cd
tassanai001 <tassanai.yeeton@gmail.com>
2025-02-06 20:24:29 +0700

    Form Submission

--- app/user/profile/profile-form.tsx
+++ app/user/profile/profile-form.tsx
@@ -2,11 +2,11 @@
 
 import { Button } from '@/components/ui/button';
 import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormMessage,
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { useToast } from '@/hooks/use-toast';
@@ -18,72 +18,93 @@ import { updateProfileSchema } from '@/lib/validator';
 import { updateProfile } from '@/lib/actions/user.actions';
 
 const ProfileForm = () => {
-  const { data: session, update } = useSession();
+    const { data: session, update } = useSession();
 
-  const form = useForm<z.infer<typeof updateProfileSchema>>({
-    resolver: zodResolver(updateProfileSchema),
-    defaultValues: {
-      name: session?.user?.name ?? '',
-      email: session?.user?.email ?? '',
-    },
-  });
+    const form = useForm<z.infer<typeof updateProfileSchema>>({
+        resolver: zodResolver(updateProfileSchema),
+        defaultValues: {
+            name: session?.user?.name ?? '',
+            email: session?.user?.email ?? '',
+        },
+    });
 
-  const { toast } = useToast();
+    const { toast } = useToast();
 
-  const onSubmit = () => {
-    return;
-  }
+    // Submit form to update profile
+    async function onSubmit(values: z.infer<typeof updateProfileSchema>) {
+        const res = await updateProfile(values);
 
-  return (
-    <Form {...form}>
-       <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="flex flex-col gap-5">
-        <div className='flex flex-col gap-5'>
-          <FormField
-            control={form.control}
-            name='email'
-            render={({ field }) => (
-              <FormItem className='w-full'>
-                <FormControl>
-                  <Input
-                    disabled
-                    placeholder='Email'
-                    {...field}
-                    className='input-field'
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name='name'
-            render={({ field }) => (
-              <FormItem className='w-full'>
-                <FormControl>
-                  <Input
-                    placeholder='Name'
-                    {...field}
-                    className='input-field'
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
-        <Button
-          type='submit'
-          size='lg'
-          disabled={form.formState.isSubmitting}
-          className='button col-span-2 w-full'
-        >
-          {form.formState.isSubmitting ? 'Submitting...' : 'Update Profile'}
-        </Button>
-      </form>
-    </Form>
-  );
+        if (!res.success)
+            return toast({
+                variant: 'destructive',
+                description: res.message,
+            });
+
+        const newSession = {
+            ...session,
+            user: {
+                ...session?.user,
+                name: values.name,
+            },
+        };
+
+        await update(newSession);
+
+        toast({
+            description: res.message,
+        });
+    }
+
+    return (
+        <Form {...form}>
+            <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="flex flex-col gap-5">
+                <div className='flex flex-col gap-5'>
+                    <FormField
+                        control={form.control}
+                        name='email'
+                        render={({ field }) => (
+                            <FormItem className='w-full'>
+                                <FormControl>
+                                    <Input
+                                        disabled
+                                        placeholder='Email'
+                                        {...field}
+                                        className='input-field'
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name='name'
+                        render={({ field }) => (
+                            <FormItem className='w-full'>
+                                <FormControl>
+                                    <Input
+                                        placeholder='Name'
+                                        {...field}
+                                        className='input-field'
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+                <Button
+                    type='submit'
+                    size='lg'
+                    disabled={form.formState.isSubmitting}
+                    className='button col-span-2 w-full'
+                >
+                    {form.formState.isSubmitting ? 'Submitting...' : 'Update Profile'}
+                </Button>
+            </form>
+        </Form>
+    );
 };
 export default ProfileForm;
--- auth.ts
+++ auth.ts
@@ -1,166 +1,166 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { compareSync } from 'bcrypt-ts-edge';
-import type { NextAuthConfig } from 'next-auth';
-import NextAuth from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
+import { compareSync } from "bcrypt-ts-edge";
+import type { NextAuthConfig } from "next-auth";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
-import { prisma } from '@/db/prisma';
-import { PrismaAdapter } from '@auth/prisma-adapter';
-import { NextResponse } from 'next/server';
-import { cookies } from 'next/headers';
+import { prisma } from "@/db/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
 
 export const config = {
-    pages: {
-      signIn: '/sign-in',
-      error: '/sign-in',
-    },
-    session: {
-      strategy: 'jwt',
-      maxAge: 30 * 24 * 60 * 60,
-    },
-    adapter: PrismaAdapter(prisma),
-    providers: [
-      CredentialsProvider({
-        credentials: {
-          email: {
-            type: 'email',
+  pages: {
+    signIn: "/sign-in",
+    error: "/sign-in",
+  },
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60,
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: {
+          type: "email",
+        },
+        password: { type: "password" },
+      },
+      async authorize(credentials) {
+        if (credentials == null) return null;
+
+        // Find user in database
+        const user = await prisma.user.findFirst({
+          where: {
+            email: credentials.email as string,
           },
-          password: { type: 'password' },
-        },
-        async authorize(credentials) {
-          if (credentials == null) return null;
-
-          // Find user in database
-          const user = await prisma.user.findFirst({
-            where: {
-              email: credentials.email as string,
-            },
-          });
-          // Check if user exists and password is correct
-          if (user && user.password) {
-            const isMatch = compareSync(
-              credentials.password as string,
-              user.password
-            );
-            // If password is correct, return user object
-            if (isMatch) {
-              return {
-                id: user.id,
-                name: user.name,
-                email: user.email,
-                role: user.role,
-              };
-            }
-          }
-          // If user doesn't exist or password is incorrect, return null
-          return null;
-        },
-      }),
-    ],
-    callbacks: {
-      async session ({ session, user, trigger, token }: any) {
-        // Set the user id on the session
-        session.user.id = token.sub;
-        session.user.name = token.name;
-        session.user.role = token.role;
-        // If there is an update, set the name on the session
-        if (trigger === 'update') {
-          session.user.name = user.name;
-        }
-        return session;
-      },
-      async jwt({ token, user, trigger, session }: any) {
-        // Assign user fields to token
-        if (user) {
-          token.id = user.id;
-          token.role = user.role;
-
-          // If user has no name, use email as their default name
-          if (user.name === 'NO_NAME') {
-            token.name = user.email!.split('@')[0];
-
-            // Update the user in the database with the new name
-            await prisma.user.update({
-              where: { id: user.id },
-              data: { name: token.name },
-            });
+        });
+        // Check if user exists and password is correct
+        if (user && user.password) {
+          const isMatch = compareSync(
+            credentials.password as string,
+            user.password
+          );
+          // If password is correct, return user object
+          if (isMatch) {
+            return {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+              role: user.role,
+            };
           }
         }
-
-        if (trigger === 'signIn' || trigger === 'signUp') {
-          const cookiesObject = await cookies();
-          const sessionCartId = cookiesObject.get('sessionCartId')?.value;
-
-          if (sessionCartId) {
-            const sessionCart = await prisma.cart.findFirst({
-              where: { sessionCartId },
-            });
-
-            if (sessionCart) {
-              // Overwrite any existing user cart
-              await prisma.cart.deleteMany({
-                where: { userId: user.id },
-              });
-
-              // Assign the guest cart to the logged-in user
-              await prisma.cart.update({
-                where: { id: sessionCart.id },
-                data: { userId: user.id },
-              });
-            }
-          }
-        }
-
-        // Handle session updates (e.g., name change)
-        if (session?.user.name && trigger === 'update') {
-          token.name = session.user.name;
-        }
-
-        return token;
+        // If user doesn't exist or password is incorrect, return null
+        return null;
       },
-      authorized({ request, auth }: any) {
-        // Array of regex patterns for protected paths
-        const protectedPaths = [
-          /\/shipping-address/,
-          /\/payment-method/,
-          /\/place-order/,
-          /\/profile/,
-          /\/user\/(.*)/,
-          /\/order\/(.*)/,
-          /\/admin/,
-        ];
-
-        // Get pathname from the req URL object
-        const { pathname } = request.nextUrl;
-
-        // Check if user is not authenticated and on a protected path
-        if (!auth && protectedPaths.some((p) => p.test(pathname))) return false;
-
-        // Check for cart cookie
-        if (!request.cookies.get('sessionCartId')) {
-          // Generate cart cookie
-          const sessionCartId = crypto.randomUUID();
-
-          // Clone the request headers
-          const newRequestHeaders = new Headers(request.headers);
-
-          // Create a new response and add the new headers
-          const response = NextResponse.next({
-            request: {
-              headers: newRequestHeaders,
-            },
-          });
-
-          // Set the newly generated sessionCartId in the response cookies
-          response.cookies.set('sessionCartId', sessionCartId);
-
-          // Return the response with the sessionCartId set
-          return response;
-        } else {
-          return true;
-        }
-      },
+    }),
+  ],
+  callbacks: {
+    async session({ session, user, trigger, token }: any) {
+      // Set the user id on the session
+      session.user.id = token.sub;
+      session.user.name = token.name;
+      session.user.role = token.role;
+      // If there is an update, set the name on the session
+      if (trigger === "update") {
+        session.user.name = user.name;
+      }
+      return session;
     },
-  } satisfies NextAuthConfig;
+    async jwt({ token, user, trigger, session }: any) {
+      // Assign user fields to token
+      if (user) {
+        token.id = user.id;
+        token.role = user.role;
+
+        // If user has no name, use email as their default name
+        if (user.name === "NO_NAME") {
+          token.name = user.email!.split("@")[0];
+
+          // Update the user in the database with the new name
+          await prisma.user.update({
+            where: { id: user.id },
+            data: { name: token.name },
+          });
+        }
+      }
+
+      if (trigger === "signIn" || trigger === "signUp") {
+        const cookiesObject = await cookies();
+        const sessionCartId = cookiesObject.get("sessionCartId")?.value;
+
+        if (sessionCartId) {
+          const sessionCart = await prisma.cart.findFirst({
+            where: { sessionCartId },
+          });
+
+          if (sessionCart) {
+            // Overwrite any existing user cart
+            await prisma.cart.deleteMany({
+              where: { userId: user.id },
+            });
+
+            // Assign the guest cart to the logged-in user
+            await prisma.cart.update({
+              where: { id: sessionCart.id },
+              data: { userId: user.id },
+            });
+          }
+        }
+      }
+
+      // Handle session updates (e.g., name change)
+      if (session?.user.name && trigger === "update") {
+        token.name = session.user.name;
+      }
+
+      return token;
+    },
+    authorized({ request, auth }: any) {
+      // Array of regex patterns for protected paths
+      const protectedPaths = [
+        /\/shipping-address/,
+        /\/payment-method/,
+        /\/place-order/,
+        /\/profile/,
+        /\/user\/(.*)/,
+        /\/order\/(.*)/,
+        /\/admin/,
+      ];
+
+      // Get pathname from the req URL object
+      const { pathname } = request.nextUrl;
+
+      // Check if user is not authenticated and on a protected path
+      if (!auth && protectedPaths.some((p) => p.test(pathname))) return false;
+
+      // Check for cart cookie
+      if (!request.cookies.get("sessionCartId")) {
+        // Generate cart cookie
+        const sessionCartId = crypto.randomUUID();
+
+        // Clone the request headers
+        const newRequestHeaders = new Headers(request.headers);
+
+        // Create a new response and add the new headers
+        const response = NextResponse.next({
+          request: {
+            headers: newRequestHeaders,
+          },
+        });
+
+        // Set the newly generated sessionCartId in the response cookies
+        response.cookies.set("sessionCartId", sessionCartId);
+
+        // Return the response with the sessionCartId set
+        return response;
+      } else {
+        return true;
+      }
+    },
+  },
+} satisfies NextAuthConfig;
 
 export const { handlers, auth, signIn, signOut } = NextAuth(config);
