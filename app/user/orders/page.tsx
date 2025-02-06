14132ef522ac72201f94a9aede831e15da770617
tassanai001 <tassanai.yeeton@gmail.com>
2025-02-06 20:05:48 +0700

    Orders Pagination

--- app/user/orders/page.tsx
+++ app/user/orders/page.tsx
@@ -1,3 +1,4 @@
+import Pagination from '@/components/shared/pagination';
 import {
     Table,
     TableBody,
@@ -63,6 +64,12 @@ const OrdersPage = async (props: {
                         ))}
                     </TableBody>
                 </Table>
+                {orders.totalPages > 1 && (
+                    <Pagination
+                        page={Number(page) || 1}
+                        totalPages={orders?.totalPages}
+                    />
+                )}
             </div>
         </div>
     );
