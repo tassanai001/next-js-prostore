9dfebc28f0c5f43a765cc7e7b83528ab2514af22
tassanai001 <tassanai.yeeton@gmail.com>
2025-02-17 20:52:42 +0700

    Create Product Handler

--- components/admin/product-form.tsx
+++ components/admin/product-form.tsx
@@ -10,21 +10,18 @@ import {
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { useToast } from '@/hooks/use-toast';
-import { createProduct, updateProduct } from '@/lib/actions/product.actions';
 import { productDefaultValues } from '@/lib/constants';
 import { insertProductSchema, updateProductSchema } from '@/lib/validator';
-import { ControllerRenderProps } from 'react-hook-form';
+import { ControllerRenderProps, SubmitHandler } from 'react-hook-form';
 import { Product } from '@/types';
 import { zodResolver } from '@hookform/resolvers/zod';
 import slugify from 'slugify';
 import { useRouter } from 'next/navigation';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
-import { Card, CardContent } from '@/components/ui/card';
-import Image from 'next/image';
-import { Checkbox } from '@/components/ui/checkbox';
 import { Textarea } from '@/components/ui/textarea';
 import { Button } from '@/components/ui/button';
+import { createProduct, updateProduct } from '@/lib/actions/product.action';
 
 const ProductForm = ({
     type,
@@ -46,9 +43,50 @@ const ProductForm = ({
         defaultValues: product && type === 'Update' ? product : productDefaultValues,
     });
 
+    // Handle form submit
+    const onSubmit: SubmitHandler<z.infer<typeof insertProductSchema>> = async (
+        values
+    ) => {
+        if (type === 'Create') {
+            const res = await createProduct(values);
+
+            if (!res.success) {
+                toast({
+                    variant: 'destructive',
+                    description: res.message,
+                });
+            } else {
+                toast({
+                    description: res.message,
+                });
+                router.push(`/admin/products`);
+            }
+        }
+        if (type === 'Update') {
+            if (!productId) {
+                router.push(`/admin/products`);
+                return;
+            }
+
+            const res = await updateProduct({ ...values, id: productId });
+
+            if (!res.success) {
+                toast({
+                    variant: 'destructive',
+                    description: res.message,
+                });
+            } else {
+                router.push(`/admin/products`);
+            }
+        }
+    };
+
     return (
         <Form {...form}>
-            <form className='space-y-8'>
+            <form
+                method='post'
+                onSubmit={form.handleSubmit(onSubmit)}
+                className='space-y-8'>
                 <div className='flex flex-col gap-5 md:flex-row'>
                     {/* Name */}
                     <FormField
