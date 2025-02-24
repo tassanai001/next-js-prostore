9794fcb3a333cf132b5e9de3cf4646b49c7dc871
tassanai001 <tassanai.yeeton@gmail.com>
2025-02-24 21:26:31 +0700

    Admin Search Form

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,9 +1,9 @@
 import { APP_NAME } from '@/lib/constants';
 import Image from 'next/image';
 import Link from 'next/link';
-import { Input } from '@/components/ui/input';
 import Menu from '@/components/shared/header/menu';
 import { MainNav } from './main-nav';
+import AdminSearch from '@/components/admin/admin-search';
 
 export default async function AdminLayout({
   children,
@@ -25,13 +25,7 @@ export default async function AdminLayout({
             </Link>
             <MainNav className='mx-6' />
             <div className='ml-auto flex items-center space-x-4'>
-              <div>
-                <Input
-                  type='search'
-                  placeholder='Search...'
-                  className='md:w-[100px] lg:w-[300px]'
-                />
-              </div>
+              <AdminSearch />
               <Menu />
             </div>
           </div>
