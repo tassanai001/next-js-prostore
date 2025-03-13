73a39f1a967a20299624ad3fa74f0c7f7d0e0ed2
tassanai001 <tassanai.yeeton@gmail.com>
2025-03-13 22:12:17 +0700

    Get Filter URL Function

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -23,6 +23,29 @@ const SearchPage = async (props: {
 
     console.log(q, category, price, rating, sort, page);
 
+    // Construct filter url
+    const getFilterUrl = ({
+        c,
+        s,
+        p,
+        r,
+        pg,
+    }: {
+        c?: string;
+        s?: string;
+        p?: string;
+        r?: string;
+        pg?: string;
+    }) => {
+        const params = { q, category, price, rating, sort, page };
+        if (c) params.category = c;
+        if (p) params.price = p;
+        if (r) params.rating = r;
+        if (pg) params.page = pg;
+        if (s) params.sort = s;
+        return `/search?${new URLSearchParams(params).toString()}`;
+    };
+
     // Get products
     const products = await getAllProducts({
         category,
@@ -35,7 +58,9 @@ const SearchPage = async (props: {
 
     return (
         <div className='grid md:grid-cols-5 md:gap-5'>
-            <div className='filter-links'>{/* FILTERS */}</div>
+            <div className='filter-links'>
+                URL: {getFilterUrl({ c: "Men's Dress Shirts" })}
+            </div>
             <div className='md:col-span-4 space-y-4'>
                 <div className='grid grid-cols-1 gap-4 md:grid-cols-3'>
                     {products!.data.length === 0 && <div>No product found</div>}
