bd6e5cf85d8d2c92a1d7d0443661646f1218b314
tassanai001 <tassanai.yeeton@gmail.com>
2025-03-24 20:28:18 +0700

    Get Reviews Action

--- lib/actions/review.actions.ts
+++ lib/actions/review.actions.ts
@@ -87,3 +87,38 @@ export async function createUpdateReview(
     };
   }
 }
+
+// Get all reviews for a product
+export async function getReviews({ productId }: { productId: string }) {
+  const data = await prisma.review.findMany({
+    where: {
+      productId: productId,
+    },
+    include: {
+      user: {
+        select: {
+          name: true,
+        },
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return { data };
+}
+
+// Get a review for a product written by the current user
+export const getReviewByProductId = async ({
+  productId,
+}: {
+  productId: string;
+}) => {
+  const session = await auth();
+  if (!session) throw new Error("User is not authenticated");
+
+  return await prisma.review.findFirst({
+    where: { productId, userId: session?.user.id },
+  });
+};
