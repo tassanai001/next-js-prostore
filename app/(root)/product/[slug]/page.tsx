d2add8b17138a05df821aa58a89605d569ca9759
tassanai001 <tassanai.yeeton@gmail.com>
2025-03-24 20:36:54 +0700

    Display Reviews

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -8,6 +8,7 @@ import AddToCart from '@/components/shared/product/add-to-cart';
 import { getMyCart } from '@/lib/actions/cart.actions';
 import ReviewList from './review-list';
 import { auth } from '@/auth';
+import Rating from '@/components/shared/product/rating';
 
 const ProductDetailsPage = async (props: {
   params: Promise<{ slug: string }>;
@@ -39,9 +40,8 @@ const ProductDetailsPage = async (props: {
                 {product.brand} {product.category}
               </p>
               <h1 className='h3-bold'>{product.name}</h1>
-              <p>
-                {product.rating.toString()} of {product.numReviews} reviews
-              </p>
+              <Rating value={Number(product.rating)} />
+              <p>{product.numReviews} reviews</p>
 
               <div className='flex flex-col gap-3 sm:flex-row sm:items-center'>
                 <ProductPrice
--- app/(root)/product/[slug]/review-list.tsx
+++ app/(root)/product/[slug]/review-list.tsx
@@ -1,9 +1,14 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Review } from '@/types';
 import Link from 'next/link';
 import ReviewForm from './review-form';
+import { getReviews } from '@/lib/actions/review.actions';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Calendar, User } from 'lucide-react';
+import { formatDateTime } from '@/lib/utils';
+import Rating from '@/components/shared/product/rating';
 
 const ReviewList = ({
     productId,
@@ -15,8 +20,16 @@ const ReviewList = ({
     productSlug: string;
 }) => {
 
-    console.log(productId, userId, productSlug);
     const [reviews, setReviews] = useState<Review[]>([]);
+
+    useEffect(() => {
+        /// Load reviews from the database
+        const loadReviews = async () => {
+            const res = await getReviews({ productId });
+            setReviews(res.data);
+        };
+        loadReviews();
+    }, [productId]);
 
     // Reload reviews when a review is submitted
     const reload = async () => {
@@ -44,7 +57,31 @@ const ReviewList = ({
                     to write a review
                 </div>
             )}
-            <div className='flex flex-col gap-3'>{/* REVIEWS HERE */}</div>
+            <div className='flex flex-col gap-3'>{
+                reviews.map((review) => (
+                    <Card key={review.id}>
+                        <CardHeader>
+                            <div className='flex-between'>
+                                <CardTitle>{review.title}</CardTitle>
+                            </div>
+                            <CardDescription>{review.description}</CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            <div className='flex space-x-4 text-sm text-muted-foreground'>
+                                <Rating value={review.rating} />
+                                <div className='flex items-center'>
+                                    <User className='mr-1 h-3 w-3' />
+                                    {review.user ? review.user.name : 'Deleted User'}
+                                </div>
+                                <div className='flex items-center'>
+                                    <Calendar className='mr-1 h-3 w-3' />
+                                    {formatDateTime(review.createdAt).dateTime}
+                                </div>
+                            </div>
+                        </CardContent>
+                    </Card>
+                ))
+            }</div>
         </div>
     );
 };
